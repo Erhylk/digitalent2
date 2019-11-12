9db913f4dfd39864d5fc13ba3cd07137ffa73a77
Muh. Khaeril Syam <erhylksym@gmail.com>
2019-11-12 11:12:29 +0800

    ...

---
+++ Coding/BLE_ESP32_DHT11/BLE_ESP32_DHT11.ino
@@ -0,0 +1,111 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <DHT.h>
+#include <iostream>
+#include <string>
+
+BLECharacteristic * pCharacteristic;
+
+ bool deviceConnected = false;
+ const int LED = 25;  // Could be different depending on the dev board.  I used the DO32 ESP32 dev board.
+ /*
+  * Definition of DHT11
+  */
+ #define DHTPIN 23 // DHT11 data pin
+ #define DHTTYPE DHT11 // sets the sensor type, in case DHT11
+ 
+ DHT dht (DHTPIN, DHTTYPE);
+
+ int humidity;
+ int temperature;
+
+ // See the following link if you want to generate your own UUIDs:
+ // https://www.uuidgenerator.net/
+
+ #define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+ #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+ #define DHTDATA_CHAR_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+     void onConnect (BLEServer * pServer) {
+       deviceConnected = true;
+     };
+
+     void onDisconnect (BLEServer * pServer) {
+       deviceConnected = false;
+     }
+ };
+
+ class MyCallbacks: public BLECharacteristicCallbacks {
+     void onWrite (BLECharacteristic * pCharacteristic) {
+       std :: string rxValue = pCharacteristic-> getValue ();
+       Serial.println (rxValue [0]);
+
+       if (rxValue.length ()> 0) {
+         Serial.println ("*********");
+         Serial.print ("Received Value:");
+
+         for (int i = 0; i  setCallbacks (new MyServerCallbacks ());
+
+         // Create the UART service
+   BLEService * pService = pServer-> createService (SERVICE_UUID);
+
+   // Create a BLE Feature to send the data
+   pCharacteristic = pService-> createCharacteristic (
+                       DHTDATA_CHAR_UUID,
+                       BLECharacteristic :: PROPERTY_NOTIFY
+                     );
+   pCharacteristic-> addDescriptor (new BLE2902 ());
+   
+// creates a BLE characteristic to receive the data
+   BLECharacteristic * pCharacteristic = pService-> createCharacteristic (
+                                          CHARACTERISTIC_UUID_RX,
+                                          BLECharacteristic :: PROPERTY_WRITE
+                                        );
+   pCharacteristic-> setCallbacks (new MyCallbacks ());
+   // Start the service
+   pService-> start ();
+
+   // Starts the discovery of ESP32
+   pServer-> getAdvertising () -> start ();
+   Serial.println ("Waiting for a client to connect ...");
+ }
+ void loop () {
+   if (deviceConnected) {
+
+     humidity = dht.readHumidity ();
+     temperature = dht.readTemperature ();
+     // test if return is valid, otherwise something is wrong.
+     if (isnan (temperature) || isnan (humidity))
+     {
+       Serial.println ("Failed to read from DHT");
+     }
+     else
+     {
+       Serial.print ("Humidity:");
+       Serial.print (humidity);
+       Serial.print ("% \ t");
+       Serial.print ("Temperature:");
+       Serial.print (temperature);
+       Serial.println ("* C");
+     }
+    
+     char humidityString [2];
+     char temperatureString [2];
+     dtostrf (humidity, 1, 2, humidityString);
+     dtostrf (temperature, 1, 2, temperatureString);
+
+     char dhtDataString [16];
+     sprintf (dhtDataString, "% d,% d", temperature, humidity);
+    
+     pCharacteristic-> setValue (dhtDataString);
+    
+     pCharacteristic-> notify ();  // Send the value to the application!
+     Serial.print ("*** Sent Data:");
+     Serial.print (dhtDataString);
+     Serial.println ("***");
+   }
+   delay (1000);
+ }
---
+++ Coding/BLE_ESP32_TEST/BLE_ESP32_TEST.ino
@@ -0,0 +1,93 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+float txValue = 0;
+const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
+const int LED = 2; // PIN LED PADA BOARD
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // MASUKKAN KODE DARI UUID
+#define CHARACTERISTIC_UUID_RX "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // MASUKKAN KODE DARI UUID
+#define CHARACTERISTIC_UUID_TX "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // MASUKKAN KODE DARI UUID
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++) { Serial.print(rxValue[i]); } Serial.println();
+        if (rxValue.find("ON") != -1) { Serial.println("Turning ON!"); digitalWrite(LED, HIGH); } else if (rxValue.find("OFF") != -1) { Serial.println("Turning OFF!"); digitalWrite(LED, LOW); } Serial.println(); Serial.println("*********"); } } }; void setup() { Serial.begin(115200); pinMode(LED, OUTPUT); // Create the BLE Device 
+        BLEDevice::init("ESP32 UART Test"); //ATUR NAMA BLUETOOTH YANG ANDA INGINKAN
+        // Create the BLE Server 
+        BLEServer *pServer = BLEDevice::createServer(); pServer->setCallbacks(new MyServerCallbacks());
+
+        // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+                    pCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+                                       pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    // Fabricate some arbitrary junk for now...
+    txValue = analogRead(readPin) / 3.456; // This could be an actual sensor reading!
+        // Let's convert the value to a char array:
+    char txString[8]; // make sure this is big enuffz
+    dtostrf(txValue, 1, 2, txString); // float_val, min_width, digits_after_decimal, char_buffer
+    
+//    pCharacteristic->setValue(&txValue, 1); // To send the integer value
+//    pCharacteristic->setValue("Hello!"); // Sending a test message
+    pCharacteristic->setValue(txString);
+    
+    pCharacteristic->notify(); // Send the value to the app!
+    Serial.print("*** Sent Value: ");
+    Serial.print(txString);
+    Serial.println(" ***");
+
+    // You can add the rxValue checks down here instead
+    // if you set "rxValue" as a global var at the top!
+    // Note you will have to delete "std::string" declaration
+    // of "rxValue" in the callback function.
+//    if (rxValue.find("ON") != -1) {
+//      Serial.println("Turning ON!");
+//      digitalWrite(LED, HIGH);
+//    }
+//    else if (rxValue.find("OFF") != -1) {
+//      Serial.println("Turning OFF!");
+//      digitalWrite(LED, LOW);
+//    }
+  }
+  delay(1000);
+}
+        
